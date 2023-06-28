e0cd12cc4201cce12762fd20330883bb8a0d9c3e
Costin Croitoru <croitorucostin939@gmail.com>
2023-06-28 18:59:44 +0300

    Add task functionality

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-const taskInput= document.getElementById("task").value
-const addTaskButton= document.getElementById("add-task")
+const taskInput = document.getElementById('task');
+const addTaskButton = document.getElementById('add-task');
+const tasksList = document.getElementById('tasks-list');
 
+function buttonClick() {
+  const taskElement = `
+  <li>${taskInput.value}</li>
+  `;
+  tasksList.insertAdjacentHTML('beforeend', taskElement);
+}
 
+addTaskButton.addEventListener('click', () => buttonClick());
